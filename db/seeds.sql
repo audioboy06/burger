b5a22685bed9be4c8b539c76763d9f1ed0903511
audioboy06 <rickjoice@Ricks-Mac-mini.local>
2018-02-08 15:04:04 -0500

    sql files done others only example code

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+    
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Avocado Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("General Tso's Crispy Chicken Sandwich", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Brunch Burger", false);
